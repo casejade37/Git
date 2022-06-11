19adf4081d0bf9959eb33f2dfd4b56c269e1984e
Jay Song <jayjiansong@gmail.com>
2022-06-10 20:49:40 -0400

    web dev

---
+++ Web Dev/my-first-app/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { RecipesComponent } from './recipes/recipes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    RecipesComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+ 
---
+++ Web Dev/prj-start/src/app/recipes/recipes.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.component.css']
+})
+export class RecipesComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'image', 'html')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
